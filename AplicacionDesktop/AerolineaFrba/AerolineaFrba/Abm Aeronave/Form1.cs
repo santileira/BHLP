7cd4c8e82b9b074600003a8a3228b1a8a3765d9e
santileira <santiagoleira10@gmail.com>
2015-10-06 20:35:42 -0300

    ABM principal de aeronave

--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Aeronave/Form1.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Aeronave/Form1.cs
@@ -12,9 +12,37 @@ namespace AerolineaFrba.Abm_Aeronave
 {
     public partial class Principal : Form
     {
+
+        Form formularioSiguiente;
+
         public Principal()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            formularioSiguiente = new Alta();
+            this.cambiarVisibilidades(formularioSiguiente);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            formularioSiguiente = new Baja();
+            this.cambiarVisibilidades(formularioSiguiente);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            formularioSiguiente = new Modificacion();
+            this.cambiarVisibilidades(formularioSiguiente);
+        }
+
+        private void cambiarVisibilidades(Form formularioSiguiente)
+        {
+            formularioSiguiente.Visible = true;
+            this.Visible = false;
+        }
+       
     }
 }
