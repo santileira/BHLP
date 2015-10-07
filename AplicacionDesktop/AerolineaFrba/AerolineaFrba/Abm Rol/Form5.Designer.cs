8b7a24f9b8d41159e8206f7a6dbfecab91e12f6e
ezebor <ezequiel.borenstein@gmail.com>
2015-10-07 15:49:20 -0300

    listado

--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Rol/Form5.Designer.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Rol/Form5.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFiltro4 = new System.Windows.Forms.TextBox();
             this.cboFiltro3 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,14 +41,26 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.dg = new System.Windows.Forms.DataGridView();
+            this.lblErrores = new System.Windows.Forms.Label();
+            this.cboFiltro2 = new System.Windows.Forms.ComboBox();
+            this.cboFiltro1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.optEstadoAlta = new System.Windows.Forms.RadioButton();
+            this.optEstadoBaja = new System.Windows.Forms.RadioButton();
+            this.chkEstadoIgnorar = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.chkEstadoIgnorar);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.cboFiltro2);
+            this.groupBox1.Controls.Add(this.cboFiltro1);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtFiltro4);
             this.groupBox1.Controls.Add(this.cboFiltro3);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
@@ -58,39 +70,41 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(22, 15);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(455, 98);
+            this.groupBox1.Size = new System.Drawing.Size(600, 176);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filtros de Busqueda";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(360, 59);
+            this.button1.Location = new System.Drawing.Point(515, 58);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(79, 20);
             this.button1.TabIndex = 8;
             this.button1.Text = "Seleccionar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtFiltro4
             // 
-            this.textBox1.Location = new System.Drawing.Point(304, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(50, 20);
-            this.textBox1.TabIndex = 7;
+            this.txtFiltro4.Enabled = false;
+            this.txtFiltro4.Location = new System.Drawing.Point(414, 59);
+            this.txtFiltro4.Name = "txtFiltro4";
+            this.txtFiltro4.Size = new System.Drawing.Size(87, 20);
+            this.txtFiltro4.TabIndex = 7;
             // 
             // cboFiltro3
             // 
+            this.cboFiltro3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboFiltro3.FormattingEnabled = true;
-            this.cboFiltro3.Location = new System.Drawing.Point(304, 24);
+            this.cboFiltro3.Location = new System.Drawing.Point(414, 25);
             this.cboFiltro3.Name = "cboFiltro3";
-            this.cboFiltro3.Size = new System.Drawing.Size(135, 21);
+            this.cboFiltro3.Size = new System.Drawing.Size(180, 21);
             this.cboFiltro3.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(232, 66);
+            this.label4.Location = new System.Drawing.Point(363, 66);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 13);
             this.label4.TabIndex = 5;
@@ -99,7 +113,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(232, 28);
+            this.label3.Location = new System.Drawing.Point(363, 28);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 4;
@@ -107,50 +121,54 @@
             // 
             // txtFiltro2
             // 
-            this.txtFiltro2.Location = new System.Drawing.Point(91, 59);
+            this.txtFiltro2.Location = new System.Drawing.Point(259, 60);
             this.txtFiltro2.Name = "txtFiltro2";
             this.txtFiltro2.Size = new System.Drawing.Size(88, 20);
             this.txtFiltro2.TabIndex = 3;
+            this.txtFiltro2.Visible = false;
             // 
             // txtFiltro1
             // 
-            this.txtFiltro1.Location = new System.Drawing.Point(91, 25);
+            this.txtFiltro1.Location = new System.Drawing.Point(259, 28);
             this.txtFiltro1.Name = "txtFiltro1";
             this.txtFiltro1.Size = new System.Drawing.Size(88, 20);
             this.txtFiltro1.TabIndex = 2;
+            this.txtFiltro1.Visible = false;
+            this.txtFiltro1.TextChanged += new System.EventHandler(this.txtFiltro1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 66);
+            this.label2.Location = new System.Drawing.Point(6, 66);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(143, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Filtro 2";
+            this.label2.Text = "Filtro por igualdad de palabra";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Location = new System.Drawing.Point(6, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(147, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Filtro 1";
+            this.label1.Text = "Filtro que contenga la palabra";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(22, 128);
+            this.button2.Location = new System.Drawing.Point(26, 197);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(91, 21);
+            this.button2.Size = new System.Drawing.Size(108, 21);
             this.button2.TabIndex = 1;
             this.button2.Text = "Limpiar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(386, 128);
+            this.button3.Location = new System.Drawing.Point(514, 197);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(91, 21);
+            this.button3.Size = new System.Drawing.Size(108, 21);
             this.button3.TabIndex = 2;
             this.button3.Text = "Buscar";
             this.button3.UseVisualStyleBackColor = true;
@@ -159,28 +177,110 @@
             // dg
             // 
             this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dg.Location = new System.Drawing.Point(26, 159);
+            this.dg.Location = new System.Drawing.Point(26, 232);
             this.dg.Name = "dg";
-            this.dg.Size = new System.Drawing.Size(450, 270);
+            this.dg.Size = new System.Drawing.Size(596, 235);
             this.dg.TabIndex = 3;
+            // 
+            // lblErrores
+            // 
+            this.lblErrores.AutoSize = true;
+            this.lblErrores.Location = new System.Drawing.Point(653, 197);
+            this.lblErrores.Name = "lblErrores";
+            this.lblErrores.Size = new System.Drawing.Size(0, 13);
+            this.lblErrores.TabIndex = 4;
+            // 
+            // cboFiltro2
+            // 
+            this.cboFiltro2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFiltro2.FormattingEnabled = true;
+            this.cboFiltro2.Items.AddRange(new object[] {
+            "ROL_ESTADO",
+            "ROL_NOMBRE"});
+            this.cboFiltro2.Location = new System.Drawing.Point(159, 59);
+            this.cboFiltro2.Name = "cboFiltro2";
+            this.cboFiltro2.Size = new System.Drawing.Size(94, 21);
+            this.cboFiltro2.TabIndex = 10;
+            this.cboFiltro2.SelectedIndexChanged += new System.EventHandler(this.cboFiltro2_SelectedIndexChanged);
+            // 
+            // cboFiltro1
+            // 
+            this.cboFiltro1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFiltro1.FormattingEnabled = true;
+            this.cboFiltro1.Items.AddRange(new object[] {
+            "ROL_ESTADO",
+            "ROL_NOMBRE"});
+            this.cboFiltro1.Location = new System.Drawing.Point(159, 27);
+            this.cboFiltro1.Name = "cboFiltro1";
+            this.cboFiltro1.Size = new System.Drawing.Size(94, 21);
+            this.cboFiltro1.TabIndex = 9;
+            this.cboFiltro1.SelectedIndexChanged += new System.EventHandler(this.cboFiltro1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.optEstadoBaja);
+            this.groupBox2.Controls.Add(this.optEstadoAlta);
+            this.groupBox2.Location = new System.Drawing.Point(9, 86);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(140, 84);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filtrar por estado";
+            // 
+            // optEstadoAlta
+            // 
+            this.optEstadoAlta.AutoSize = true;
+            this.optEstadoAlta.Location = new System.Drawing.Point(6, 29);
+            this.optEstadoAlta.Name = "optEstadoAlta";
+            this.optEstadoAlta.Size = new System.Drawing.Size(88, 17);
+            this.optEstadoAlta.TabIndex = 13;
+            this.optEstadoAlta.TabStop = true;
+            this.optEstadoAlta.Text = "Estado ALTA";
+            this.optEstadoAlta.UseVisualStyleBackColor = true;
+            // 
+            // optEstadoBaja
+            // 
+            this.optEstadoBaja.AutoSize = true;
+            this.optEstadoBaja.Location = new System.Drawing.Point(6, 52);
+            this.optEstadoBaja.Name = "optEstadoBaja";
+            this.optEstadoBaja.Size = new System.Drawing.Size(87, 17);
+            this.optEstadoBaja.TabIndex = 14;
+            this.optEstadoBaja.TabStop = true;
+            this.optEstadoBaja.Text = "Estado BAJA";
+            this.optEstadoBaja.UseVisualStyleBackColor = true;
+            // 
+            // chkEstadoIgnorar
+            // 
+            this.chkEstadoIgnorar.AutoSize = true;
+            this.chkEstadoIgnorar.Location = new System.Drawing.Point(173, 115);
+            this.chkEstadoIgnorar.Name = "chkEstadoIgnorar";
+            this.chkEstadoIgnorar.Size = new System.Drawing.Size(154, 17);
+            this.chkEstadoIgnorar.TabIndex = 15;
+            this.chkEstadoIgnorar.Text = "Ignorar el estado para filtrar";
+            this.chkEstadoIgnorar.UseVisualStyleBackColor = true;
             // 
             // Listado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 448);
+            this.ClientSize = new System.Drawing.Size(887, 448);
+            this.Controls.Add(this.lblErrores);
             this.Controls.Add(this.dg);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Listado";
             this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Listado";
             this.Load += new System.EventHandler(this.Listado_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -192,12 +292,19 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFiltro4;
         private System.Windows.Forms.ComboBox cboFiltro3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridView dg;
+        private System.Windows.Forms.Label lblErrores;
+        private System.Windows.Forms.ComboBox cboFiltro2;
+        private System.Windows.Forms.ComboBox cboFiltro1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton optEstadoBaja;
+        private System.Windows.Forms.RadioButton optEstadoAlta;
+        private System.Windows.Forms.CheckBox chkEstadoIgnorar;
     }
 }
--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Rol/Form5.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Rol/Form5.cs
@@ -21,8 +21,117 @@ namespace AerolineaFrba.Abm_Rol
 
         private void Listado_Load(object sender, EventArgs e)
         {
-         //   string queryselect = "SELECT * FROM [ABSTRACCIONX4].[ROLES]";
-            string queryselect = "SELECT top 10 Ruta_Ciudad_Origen FROM gd_esquema.Maestra";
+            this.iniciar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!this.datosEntradaErroneos())
+            {
+                bool huboCondicion = false;
+
+                string queryselect = "SELECT * FROM [ABSTRACCIONX4].[ROLES]";
+           
+                if (this.sePusoFiltro())
+                    queryselect = queryselect + " WHERE ";
+
+                if (txtFiltro1.TextLength != 0)
+                {
+                    string condicion = cboFiltro1.Text + " LIKE '%" + txtFiltro1.Text + "%'";
+                    this.generarQuery(ref huboCondicion, ref queryselect, condicion);
+                }
+
+                if (txtFiltro2.TextLength != 0)
+                {
+                    string condicion = cboFiltro2.Text + "= '" + txtFiltro2.Text + "'";
+                    this.generarQuery(ref huboCondicion, ref queryselect, condicion);
+                }
+
+                if (chkEstadoIgnorar.Checked == false)
+
+                this.ejecutarConsulta(queryselect);
+            }
+            else
+                MessageBox.Show("Los datos de entrada son incorrectos", "Error en la consulta", MessageBoxButtons.OK);
+
+        }
+
+        private Boolean sePusoFiltro()
+        {
+            return (txtFiltro1.TextLength != 0 || txtFiltro2.TextLength != 0 || cboFiltro3.SelectedIndex != -1);          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.iniciar();
+        }
+
+        private Boolean datosEntradaErroneos()
+        {
+            //FILTRO 1
+      //      if (txtFiltro1.TextLength == 0)
+        
+
+
+
+            return (txtFiltro1.TextLength == 0 && txtFiltro2.TextLength == 0 && cboFiltro1.SelectedIndex == -1 && cboFiltro2.SelectedIndex == -1 && cboFiltro3.SelectedIndex == -1);
+        }
+
+        private void generarQuery(ref Boolean huboCondicion, ref string queryselect, string condicion)
+        {
+           if (huboCondicion)
+               queryselect += " AND " + condicion;
+           else
+           {
+               queryselect += condicion;
+               huboCondicion = true;
+           }
+        }
+
+        private void ejecutarConsulta(string query)
+        {
+            SqlCommand command = new SqlCommand();
+
+            DataTable t = new DataTable("Busqueda");
+            SqlDataAdapter a = new SqlDataAdapter(query, Program.conexion);
+            //Llenar el Dataset
+            DataSet ds = new DataSet();
+            a.Fill(ds, "Busqueda");
+            //Ligar el datagrid con la fuente de datos
+            dg.DataSource = ds;
+            dg.DataMember = "Busqueda";
+
+        }
+
+        private void cboFiltro1_SelectedIndexChanged(object sender, EventArgs e)
+        {/*
+            this.aplicarVisibilidad(cboFiltro1, txtFiltro1, chkFiltro1)
+            if (cboFiltro1.Text == "ROL_NOMBRE")
+            {
+                txtFiltro1.Visible = true;
+                chkFiltro1.Visible = false;
+            }
+            else if (cboFiltro1.Text == "ROL_ESTADO")
+            {
+                txtFiltro1.Visible = false;
+                chkFiltro1.Visible = true;
+            }*/
+        }
+
+        private void cboFiltro2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFiltro1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iniciar()
+        {
+            string queryselect = "SELECT * FROM [ABSTRACCIONX4].[ROLES]";
+
             DataTable t = new DataTable("Busqueda");
             SqlDataAdapter a = new SqlDataAdapter(queryselect, Program.conexion);
             //Llenar el Dataset
@@ -31,37 +140,19 @@ namespace AerolineaFrba.Abm_Rol
             //Ligar el datagrid con la fuente de datos
             dg.DataSource = ds;
             dg.DataMember = "Busqueda";
-        }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            //string queryselect = "SELECT * FROM [ABSTRACCIONX4].[ROLES]";
-            string queryselect = "SELECT top 10 Ruta_Ciudad_Origen FROM gd_esquema.Maestra";
+            chkEstadoIgnorar.Checked = true;
+            optEstadoAlta.Checked = false;
+            optEstadoBaja.Checked = false;
 
-            if(this.sePusoFiltro())
-               queryselect = queryselect + " WHERE ";
-            /*
-             * SE CONCATENAN LOS CRITERIOS 
-                        if (txtFiltro1.TextLength != 0)
-                            queryselect = queryselect + dg.NOMBRE_COLUMNA + " LIKE " + txtFiltro1.Text;
-            
-                        if(txtFiltro2.TextLength != 0)
-                            queryselect = queryselect + "";
-             * 
-             *        
-            */
+            txtFiltro1.Text = "";
+            txtFiltro2.Text = "";
+            txtFiltro4.Text = "";
 
-        }
-
-        private Boolean sePusoFiltro()
-        {
-            if (txtFiltro1.TextLength != 0 || txtFiltro2.TextLength != 0 || cboFiltro3.SelectedIndex != -1)
-                return true;
-            
-            return false;
+            cboFiltro1.SelectedIndex = -1;
+            cboFiltro2.SelectedIndex = -1;
+            cboFiltro3.SelectedIndex = -1;
             
         }
-
-
     }
 }
