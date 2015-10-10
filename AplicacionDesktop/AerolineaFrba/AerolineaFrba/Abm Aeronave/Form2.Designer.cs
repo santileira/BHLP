627bbd185a8836a5789daad0435b359b595dc0ab
Eze <ezequiel.borenstein@gmail.com>
2015-10-10 16:36:23 +0200

    cree el form de alta en aeronave

--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Aeronave/Form2.Designer.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Aeronave/Form2.Designer.cs
@@ -28,19 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(18, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(587, 329);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Campos del AERONAVE";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(127, 29);
+            this.txtNombre.MaxLength = 60;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(110, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre del Rol";
+            // 
+            // button4
+            // 
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.button4.Location = new System.Drawing.Point(2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(46, 22);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Atrás";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Alta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(625, 362);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Alta";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ABM Alta";
+            this.Load += new System.EventHandler(this.Alta_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+
     }
 }
--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Aeronave/Form2.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Aeronave/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,159 @@ namespace AerolineaFrba.Abm_Aeronave
 {
     public partial class Alta : Form
     {
+        SqlCommand command = new SqlCommand();
+        Form formularioSiguiente;
         public Alta()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.iniciar();
+        }
+
+        private void iniciar()
+        {
+            txtNombre.Text = "";
+            lstFuncionalidadesActuales.Items.Clear();
+            txtNombre.Focus();
+
+            string queryselect = "SELECT FUNC_DESC FROM [ABSTRACCIONX4].[FUNCIONALIDADES]";
+            command = new SqlCommand(queryselect, Program.conexion());
+            SqlDataAdapter a = new SqlDataAdapter(command);
+            DataTable t = new DataTable();
+            //Llenar el Dataset
+            a.Fill(t);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.datosCorrectos())
+            {
+                MessageBox.Show("El nombre ingresado es correcto. Se procede a dar de alta al nuevo rol", "Alta de roles", MessageBoxButtons.OK);
+                string cadenaComando = "insert into [ABSTRACCIONX4].[ROLES] (ROL_ESTADO, ROL_NOMBRE) values (1, '" + txtNombre.Text + "')";
+                this.ejecutarCommand(cadenaComando);
+
+                foreach(String funcion in lstFuncionalidadesActuales.Items)
+                {
+                    cadenaComando = "insert into [ABSTRACCIONX4].[FUNCIONES_ROLES] (ROL_COD, FUNC_COD) values ((SELECT ROL_COD FROM [ABSTRACCIONX4].[ROLES] WHERE ROL_NOMBRE = '" + txtNombre.Text + "'), (SELECT FUNC_COD FROM [ABSTRACCIONX4].[FUNCIONALIDADES] WHERE FUNC_DESC = '" + funcion + "'))";
+
+                    MessageBox.Show("El nombre ingresado es correcto. Se procede a dar de alta al nuevo rol", "Alta de roles", MessageBoxButtons.OK);
+                    this.ejecutarCommand(cadenaComando);
+ 
+                }
+            }
+        }
+
+        private Boolean datosCorrectos()
+        {
+            Boolean huboErroresEnText = this.validarTextNombre();
+            Boolean huboErroresEnList = false;
+
+            if (lstFuncionalidadesActuales.Items.Count > 0 && huboErroresEnText)
+            {
+                MessageBox.Show("El nombre no puede estar en blanco", "Error en el nombre", MessageBoxButtons.OK);
+                huboErroresEnList = true;
+            }
+
+            return !(huboErroresEnText || huboErroresEnList);
+        }
+
+        private Boolean esTexto(TextBox txt)
+        {
+            String textPattern = "[A-Za-z]";
+            System.Text.RegularExpressions.Regex regexTexto = new System.Text.RegularExpressions.Regex(textPattern);
+
+            return regexTexto.IsMatch(txt.Text);
+        }
+
+        private Boolean esNumero(TextBox txt)
+        {
+            String numericPattern = "[0-9]";
+            System.Text.RegularExpressions.Regex regexNumero = new System.Text.RegularExpressions.Regex(numericPattern);
+
+            return regexNumero.IsMatch(txt.Text);
+        }
+
+        private void Alta_Load(object sender, EventArgs e)
+        {
+            this.iniciar();
+
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandTimeout = 0;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.agregarALaLista(lstFuncionalidadesTotales, lstFuncionalidadesActuales);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.eliminarDeLaLista(lstFuncionalidadesActuales);
+        }
+
+        private void eliminarDeLaLista(ListBox lista)
+        {
+            if (lista.SelectedIndex != -1)
+                lista.Items.RemoveAt(lista.SelectedIndex);
+        }
+
+        private void agregarALaLista(ListBox lista1, ListBox lista2)
+        {
+            string valor = lista1.Text;
+            if (!lista2.Items.Contains(valor))
+                lista2.Items.Add(lista1.Text);
+        }
+
+        private void ejecutarCommand(string cadenaComando)
+        {
+            command.CommandText = cadenaComando;
+
+            try
+            {
+                command.ExecuteReader().Close();
+                MessageBox.Show("Se dio de alta correctamente al rol " + txtNombre.Text, "Alta de roles", MessageBoxButtons.OK);
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                MessageBox.Show("Ocurrio un error al intentar dar de alta. Verifique que el rol ingresado no se encuentre ya cargado", "Error en el Alta del nuevo rol", MessageBoxButtons.OK);
+            }
+        }
+
+        private Boolean validarTextNombre()
+        {
+            Boolean huboErrores = false;
+
+            if (txtNombre.TextLength == 0)
+            {
+                MessageBox.Show("El nombre no puede estar en blanco", "Error en el nombre", MessageBoxButtons.OK);
+                huboErrores = true;
+            }
+
+            if (txtNombre.TextLength > 60)
+            {
+                MessageBox.Show("El nombre debe tener a lo sumo 60 caracteres", "Error en el nombre", MessageBoxButtons.OK);
+                huboErrores = true;
+            }
+
+            if (!this.esTexto(txtNombre))
+            {
+                MessageBox.Show("El nombre debe ser una cadena de caracteres", "Error en el nombre", MessageBoxButtons.OK);
+                huboErrores = true;
+            }
+
+            return huboErrores;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            formularioSiguiente = new Principal();
+            formularioSiguiente.Visible = true;
+            this.Visible = false;
         }
     }
 }
