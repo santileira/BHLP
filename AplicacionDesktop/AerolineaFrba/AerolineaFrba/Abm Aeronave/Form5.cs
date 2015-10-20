a05d10a42bb3a4f079680b936fc9e99b5f7e9624
ezebor <ezequiel.borenstein@gmail.com>
2015-10-20 20:43:55 -0300

    arreglos

--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Aeronave/Form5.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Abm Aeronave/Form5.cs
@@ -357,9 +357,9 @@ namespace AerolineaFrba.Abm_Aeronave
 
             if (llamadoDesdeModificacion)
             {
-
                 cambiarVisibilidades(this.siguiente);
                 (siguiente as Modificacion).seSelecciono(dg.SelectedRows[0]);
+                return;
             }
             if (seSeteaQuery)
             {
@@ -371,7 +371,7 @@ namespace AerolineaFrba.Abm_Aeronave
                 (anterior as Generacion_Viaje.Form1).seSeleccionoAeronave(dg.SelectedRows[0]);
             }
 
- 
+            this.cambiarVisibilidades(this.anterior);
         }
     }
 }
