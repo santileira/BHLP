4141b6011c5614999b483c9f64d075826fc07dae
ezebor <ezequiel.borenstein@gmail.com>
2015-10-20 01:15:52 -0300

    arreglos

--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Generacion Viaje/Form1.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Generacion Viaje/Form1.cs
@@ -15,6 +15,8 @@ namespace AerolineaFrba.Generacion_Viaje
         public Abm_Aeronave.Listado listadoAeronaves;
         public Abm_Ruta.Listado listadoRutas;
 
+        Boolean sePuedeGuardar = false;
+
         Form formularioSiguiente;
 
         public Form1()
@@ -30,9 +32,9 @@ namespace AerolineaFrba.Generacion_Viaje
         private void inicio()
         {
             dateTimePicker1.Format = DateTimePickerFormat.Custom;
-            dateTimePicker1.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            dateTimePicker1.CustomFormat = "dd/mm/yyyy - HH:mm";
             dateTimePicker2.Format = DateTimePickerFormat.Custom;
-            dateTimePicker2.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            dateTimePicker2.CustomFormat = "dd/mm/yyyy - HH:mm";
 
             dateTimePicker1.Value = DateTime.Now;
             dateTimePicker2.Value = DateTime.Now;
@@ -41,35 +43,42 @@ namespace AerolineaFrba.Generacion_Viaje
             txtRuta.Text = "";
 
             this.listadoAeronaves.serv_cod = null;
-            this.listadoAeronaves.extenderQuery();
-            this.listadoAeronaves.ejecutarConsulta();
-
             this.listadoRutas.serv_cod = null;
-            this.listadoRutas.generarQueryInicial();
-            this.listadoRutas.ejecutarQuery();
+
+            dateTimePicker2.Enabled = false;
+            button5.Enabled = false;
+
+            sePuedeGuardar = false;
         }
 
         public void seSeleccionoAeronave(DataGridViewRow registro)
         {
             txtMatricula.Text = registro.Cells["AERO_MATRI"].Value.ToString();
             this.listadoRutas.serv_cod = registro.Cells["SERV_COD"].Value.ToString();
-            this.listadoRutas.inicio();
         }
 
         public void seSeleccionoRuta(DataGridViewRow registro)
         {
             txtRuta.Text = registro.Cells["RUTA_ID"].Value.ToString();
             this.listadoAeronaves.serv_cod = registro.Cells["SERV_COD"].Value.ToString();
-            this.listadoAeronaves.inicio();
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            this.cambiarVisibilidades(this.listadoAeronaves);
+            if (this.fechasErroneas())
+                MessageBox.Show("Verifique que las fechas de salida y llegada ingresadas sean correctas", "Error en los datos de entrada", MessageBoxButtons.OK);
+            else
+            {
+                this.listadoAeronaves.extenderQuery();
+                this.listadoAeronaves.ejecutarConsulta();
+                this.cambiarVisibilidades(this.listadoAeronaves);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.listadoRutas.generarQueryInicial();
+            this.listadoRutas.ejecutarQuery();
             this.cambiarVisibilidades(this.listadoRutas);
         }
 
@@ -93,13 +102,62 @@ namespace AerolineaFrba.Generacion_Viaje
 
         private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
         {
-            this.listadoAeronaves.fechaLlegada = dateTimePicker2.Value;
             button5.Enabled = true;
+
+            if (!fechasErroneas())
+                this.listadoAeronaves.fechaLlegada = dateTimePicker2.Value;      
         }
+
+        private Boolean fechasErroneas()
+        {
+            Boolean huboError = false;
+
+            if (DateTime.Compare(dateTimePicker1.Value, dateTimePicker2.Value) == 1)
+            {
+                huboError = true;
+                MessageBox.Show("La fecha de llegada no puede ser anterior a la fecha de salida");
+            }
+
+            TimeSpan diferencia = dateTimePicker2.Value - dateTimePicker1.Value;
+
+            if (diferencia.Days >= 1)
+            {
+                huboError = true;
+                MessageBox.Show("Las aeronaves tardan como mucho 24 hs en llegar a destino");
+            }
+
+            return huboError;
+        }
+
 
         private void button3_Click(object sender, EventArgs e)
         {
             this.inicio();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Boolean huboError = false;
+
+            if (txtMatricula.Text == "")
+            {
+                huboError = true;
+                MessageBox.Show("Debe seleccionar una aeronave", "Error en los datos de entrada", MessageBoxButtons.OK);
+            }
+            if (txtRuta.Text == "")
+            {
+                huboError = true;
+                MessageBox.Show("Debe seleccionar una ruta aerea", "Error en los datos de entrada", MessageBoxButtons.OK);
+            }
+            if (this.fechasErroneas())
+            {
+                huboError = true;
+                MessageBox.Show("Verifique que las fechas de salida y llegada ingresadas sean correctas", "Error en los datos de entrada", MessageBoxButtons.OK);
+            }
+            
+            if(!huboError)
+            {
+            }
         }
 
     }
