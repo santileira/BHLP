3d4ac25d6c0fef2620297a8c7a388f1b995dd767
ezebor <ezequiel.borenstein@gmail.com>
2015-10-31 21:11:14 -0300

    diseño estadisticas

--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Listado Estadistico/Form1.Designer.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Listado Estadistico/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.cboEstadistica = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -37,13 +38,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.cboSemestre = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.SlateGray;
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.cboEstadistica);
@@ -55,16 +58,28 @@
             this.groupBox1.Controls.Add(this.cboSemestre);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(754, 383);
+            this.groupBox1.Size = new System.Drawing.Size(667, 430);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(519, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 32);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Limpiar pantalla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(240, 169);
+            this.button1.Location = new System.Drawing.Point(179, 214);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(466, 32);
+            this.button1.Size = new System.Drawing.Size(148, 32);
             this.button1.TabIndex = 15;
             this.button1.Text = "Calcular Estadistica";
             this.button1.UseVisualStyleBackColor = true;
@@ -73,7 +88,7 @@
             // cboEstadistica
             // 
             this.cboEstadistica.BackColor = System.Drawing.Color.SteelBlue;
-            this.cboEstadistica.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboEstadistica.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboEstadistica.ForeColor = System.Drawing.Color.White;
             this.cboEstadistica.FormattingEnabled = true;
             this.cboEstadistica.Items.AddRange(new object[] {
@@ -82,21 +97,21 @@
             "Top 5 de los clientes con mas puntos acumulados a la fecha",
             "Top 5 de los destinos con pasajes cancelados",
             "Top 5 de las aeronaves con mayor cantidad de dias fuera de servicio"});
-            this.cboEstadistica.Location = new System.Drawing.Point(240, 125);
+            this.cboEstadistica.Location = new System.Drawing.Point(179, 163);
             this.cboEstadistica.Name = "cboEstadistica";
-            this.cboEstadistica.Size = new System.Drawing.Size(466, 27);
+            this.cboEstadistica.Size = new System.Drawing.Size(466, 26);
             this.cboEstadistica.TabIndex = 14;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(28, 129);
+            this.label3.Location = new System.Drawing.Point(38, 167);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(171, 18);
+            this.label3.Size = new System.Drawing.Size(138, 18);
             this.label3.TabIndex = 13;
-            this.label3.Text = "Seleccione una estadistica";
+            this.label3.Text = "Una estadística:";
             // 
             // dg
             // 
@@ -104,82 +119,87 @@
             this.dg.AllowUserToDeleteRows = false;
             this.dg.AllowUserToResizeColumns = false;
             this.dg.AllowUserToResizeRows = false;
-            this.dg.BackgroundColor = System.Drawing.Color.SteelBlue;
-            this.dg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dg.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dg.BackgroundColor = System.Drawing.Color.SlateGray;
+            this.dg.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dg.GridColor = System.Drawing.Color.SteelBlue;
-            this.dg.Location = new System.Drawing.Point(240, 218);
+            this.dg.Enabled = false;
+            this.dg.GridColor = System.Drawing.Color.SlateGray;
+            this.dg.Location = new System.Drawing.Point(179, 262);
             this.dg.MultiSelect = false;
             this.dg.Name = "dg";
             this.dg.ReadOnly = true;
+            this.dg.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
             this.dg.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dg.Size = new System.Drawing.Size(466, 99);
+            this.dg.Size = new System.Drawing.Size(466, 147);
             this.dg.TabIndex = 12;
+            this.dg.VirtualMode = true;
+            this.dg.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg_CellContentClick);
             // 
             // txtAnio
             // 
             this.txtAnio.BackColor = System.Drawing.Color.SteelBlue;
-            this.txtAnio.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnio.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtAnio.ForeColor = System.Drawing.Color.White;
-            this.txtAnio.Location = new System.Drawing.Point(240, 81);
+            this.txtAnio.Location = new System.Drawing.Point(179, 117);
             this.txtAnio.MaxLength = 2500;
             this.txtAnio.Name = "txtAnio";
-            this.txtAnio.Size = new System.Drawing.Size(466, 26);
+            this.txtAnio.Size = new System.Drawing.Size(466, 31);
             this.txtAnio.TabIndex = 11;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(28, 83);
+            this.label2.Location = new System.Drawing.Point(38, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 18);
+            this.label2.Size = new System.Drawing.Size(71, 18);
             this.label2.TabIndex = 10;
-            this.label2.Text = "Ingrese un Año";
+            this.label2.Text = "Un año:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(28, 37);
+            this.label1.Location = new System.Drawing.Point(38, 75);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 18);
+            this.label1.Size = new System.Drawing.Size(116, 18);
             this.label1.TabIndex = 9;
-            this.label1.Text = "Seleccione un Semestre";
+            this.label1.Text = "Un semestre:";
             // 
             // cboSemestre
             // 
             this.cboSemestre.BackColor = System.Drawing.Color.SteelBlue;
-            this.cboSemestre.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboSemestre.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboSemestre.ForeColor = System.Drawing.Color.White;
             this.cboSemestre.FormattingEnabled = true;
             this.cboSemestre.Items.AddRange(new object[] {
             "1° Semestre",
             "2° Semestre"});
-            this.cboSemestre.Location = new System.Drawing.Point(240, 33);
+            this.cboSemestre.Location = new System.Drawing.Point(179, 71);
             this.cboSemestre.Name = "cboSemestre";
-            this.cboSemestre.Size = new System.Drawing.Size(466, 27);
+            this.cboSemestre.Size = new System.Drawing.Size(466, 26);
             this.cboSemestre.TabIndex = 8;
             // 
-            // button2
+            // label4
             // 
-            this.button2.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(557, 337);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 32);
-            this.button2.TabIndex = 16;
-            this.button2.Text = "Limpiar pantalla";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(38, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 18);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Seleccione";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(780, 407);
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(701, 454);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -204,6 +224,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cboSemestre;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
 
 
     }
--- AplicacionDesktop/AerolineaFrba/AerolineaFrba/Listado Estadistico/Form1.cs
+++ AplicacionDesktop/AerolineaFrba/AerolineaFrba/Listado Estadistico/Form1.cs
@@ -26,12 +26,14 @@ namespace AerolineaFrba.Listado_Estadistico
         {
             cboSemestre.SelectedIndex = 0;
             cboEstadistica.SelectedIndex = 0;
+            txtAnio.Text = "";
             dg.Visible = false;
             
             DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
             columnHeaderStyle.BackColor = Color.Beige;
             columnHeaderStyle.Font = new Font("Calibri", 12, FontStyle.Bold);
             dg.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+            dg.CurrentCell = null;
 
         }
 
@@ -64,13 +66,31 @@ namespace AerolineaFrba.Listado_Estadistico
             if (!huboError)
             {
                 SQLManager.ejecutarQuery("select * from " + this.estadisticaSeleccionada() + "(" + this.semestreSeleccionado() + ", '" + txtAnio.Text + "')", dg);
-                dg.Visible = true;
+                dg.CurrentCell = null;
+
+                if (dg.RowCount == 0)
+                {
+                    MessageBox.Show("No se encontraron registros para armar el TOP 5", "Estadistica vacia", MessageBoxButtons.OK);
+                    dg.Visible = false;
+                }
+                else
+                    dg.Visible = true;
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             this.inicio();
+        }
+
+        private void dg_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
